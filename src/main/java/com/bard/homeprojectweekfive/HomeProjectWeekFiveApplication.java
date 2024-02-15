86ed4f969e5ee15409ede4f94b11b064d28ecff8
bard <bartoszslesar@gmail.com>
2024-02-15 19:36:21 +0100

    project initialization

---
+++ src/main/java/com/bard/homeprojectweekfive/HomeProjectWeekFiveApplication.java
@@ -0,0 +1,13 @@
+package com.bard.homeprojectweekfive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeProjectWeekFiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeProjectWeekFiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bard/homeprojectweekfive/HomeProjectWeekFiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bard.homeprojectweekfive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeProjectWeekFiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
